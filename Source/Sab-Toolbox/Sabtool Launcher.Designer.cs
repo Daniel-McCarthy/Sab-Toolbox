204bc927a6c8adbc670a899cc99d7e0b2ea84281
Dan McCarthy <wererabbbit@yahoo.com>
2017-08-27 01:07:06 -0500

    Renamed button1 to megapacksEditor

--- Source/Sab-Toolbox/Sabtool Launcher.Designer.cs
+++ Source/Sab-Toolbox/Sabtool Launcher.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Sabtool_Launcher));
-            this.button1 = new System.Windows.Forms.Button();
+            this.megapacksEditor = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
@@ -58,16 +58,16 @@
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // megapacksEditor
             // 
-            this.button1.Location = new System.Drawing.Point(21, 369);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(338, 65);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Megapack";
-            this.toolTip1.SetToolTip(this.button1, "Contains the game\'s assets, including mesh, textures, and animations (.megapack)");
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            this.megapacksEditor.Location = new System.Drawing.Point(21, 369);
+            this.megapacksEditor.Name = "megapacksEditor";
+            this.megapacksEditor.Size = new System.Drawing.Size(338, 65);
+            this.megapacksEditor.TabIndex = 1;
+            this.megapacksEditor.Text = "Megapack";
+            this.toolTip1.SetToolTip(this.megapacksEditor, "Contains the game\'s assets, including mesh, textures, and animations (.megapack)");
+            this.megapacksEditor.UseVisualStyleBackColor = true;
+            this.megapacksEditor.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
             // 
             // button2
             // 
@@ -266,7 +266,7 @@
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.megapacksEditor);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
@@ -292,7 +292,7 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button megapacksEditor;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
