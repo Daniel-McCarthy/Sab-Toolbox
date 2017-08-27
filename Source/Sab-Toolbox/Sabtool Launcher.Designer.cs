f82da99e5b1f44efb26e7001855a5cd50efb64ab
Dan McCarthy <wererabbbit@yahoo.com>
2017-08-27 01:08:17 -0500

    Renamed button2 to luaScriptsEditor

--- Source/Sab-Toolbox/Sabtool Launcher.Designer.cs
+++ Source/Sab-Toolbox/Sabtool Launcher.Designer.cs
@@ -31,7 +31,7 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Sabtool_Launcher));
             this.megapacksEditor = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.luaScriptsEditor = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
@@ -69,16 +69,16 @@
             this.megapacksEditor.UseVisualStyleBackColor = true;
             this.megapacksEditor.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
             // 
-            // button2
+            // luaScriptsEditor
             // 
-            this.button2.Location = new System.Drawing.Point(21, 298);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(338, 65);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Lua Scripts";
-            this.toolTip1.SetToolTip(this.button2, "Script files that define the game\'s missions and behavior (.lua)");
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.luaScriptsEditor.Location = new System.Drawing.Point(21, 298);
+            this.luaScriptsEditor.Name = "luaScriptsEditor";
+            this.luaScriptsEditor.Size = new System.Drawing.Size(338, 65);
+            this.luaScriptsEditor.TabIndex = 2;
+            this.luaScriptsEditor.Text = "Lua Scripts";
+            this.toolTip1.SetToolTip(this.luaScriptsEditor, "Script files that define the game\'s missions and behavior (.lua)");
+            this.luaScriptsEditor.UseVisualStyleBackColor = true;
+            this.luaScriptsEditor.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -265,7 +265,7 @@
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.luaScriptsEditor);
             this.Controls.Add(this.megapacksEditor);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.menuStrip1);
@@ -293,7 +293,7 @@
 
         #endregion
         private System.Windows.Forms.Button megapacksEditor;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button luaScriptsEditor;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
